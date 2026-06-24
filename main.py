71ae75af9d7196bd4095ba153049f0bdace99955
robertwhite22 <robertwhite.dev@gmail.com>
2026-06-24 13:43:51 -0400

    updated sheet ids

--- main.py
+++ main.py
@@ -13,21 +13,28 @@ SCOPES = [
 ]
 
 SHEET_IDS = {
-    'All Vendors': '1W-AGqIXwcqL7clDHad43hFmpPrrXzNUDYC4-dVGpngo',
     "Adored Beast": "1HwOxpAzI_HlntVVfOqxBVAWDy7cznPxxhUqOR5cy6ng",
     "Aroma Paws": "1fTvxu-y3rVpvxkt8elR1bZMECReblLE0zmYQDr1ePPg",
     "Bradley Caldwell": "1eqENDXTdDJVKdos-VUXYNYMNM806rNcDrv63Q654nyc",
+    "Brilliant Salmon Oil": "1ZJg6pPEf502MZVGCvuQcxDFHXMhWep0xdyA4xlj92aw",
     "Butchers Block": "1nDtvvDVu9tAzR2iDB4uMpUG3rcN3Fm_v09WJw3jvbJI",
     "Canine Caviar": "1TJXe9V_aF1A1wm_O9XK_iWJNU119iH3ZBBorUlX_0ss",
     "Colorado Pet Treats": "1U4nQGJvgyPWLST96Y6a4yrgJ2jGGicS6p4f7jesL8p8",
     "Dexy Paws": "1fux791xEUU9shk2kyyFK6B-f6sjJlx3zser-jq9oHYU",
+    "Evangers": "1Lg9-ar14KHJDgWjFGqFbhuh-1-OM_BpA3ABIaoWe_gE",
     "Fluff & Tuff": "1nGWM9Lt34e3vpqaETjPeMVsCTKVC9kIEQ3VVx1mEUqY",
     "Front Porch Pets": "1CyW8rNNWzmYH9iqVRgN5iTWCiqgd-cJnrAJGktGS2a0",
+    "Glacier Peak": "1iLFcfirV-2knXYGDqEE4721DtaRP_KFDgmtO-WsChMo",
+    "Great Lakes": "1ajpaKEq8XR-M_Wu9m_VOhMfmHkjjmOXYfGWENg8j02s",
+    "Homeopet": "1O35i1E_1lWxOkTJTvaURW2_qwlVO2YGaoBRfFvcstjE",
     "InClover": "1GJX-rqphRYAHM50HKrXhE3qG3ZUeB9kP0njwcuM56co",
     "Kennel Master": "1YgbCH_UxFZYAKnyJRki1ReNIdgqyUHtPS8gztUbpJaQ",
+    "Nordic Naturals": "1QvApqLGh0uFcRbbNLkpxcyqMdihM_zrc2cvvJEJ9YEg",
+    "Playology": "1crFl1pFzMluFAcUTuMcrTaAJGETtua3iU8L8HIELny8",
     "Polka Dog": "1JUFN_ErS6FXUKD9gv_RzccxJplwpEDiaX3Am4LW0shw",
     "SE": "1O6HWGeLgtdScnJ0_pQc8asaSj3-L4pP9vjCvvXa26vQ",
-    # Add a line for each vendor
+    "Tuesdays Natural Dog": "1f_iWF48FflsFBlVkR3P5Sk49Q87Q8Fpl8tklKYKsHtk",
+    "Zenta": "1x1mH8ldOwNLXOLtf8RXhSHQliOUO9mNZmHtliTpPWKw"
 }
 
 store_map = {
